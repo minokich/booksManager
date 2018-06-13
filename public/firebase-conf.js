e16fd0342ebd2553394651bb91e72cf30df777c5
minokich <40020414+minokich@users.noreply.github.com>
2018-06-13 18:21:08 +0900

    saisyo

---
+++ public/firebase-conf.js
@@ -0,0 +1,9 @@
+var config = {
+   apiKey: "your_api_key",
+   authDomain: "your-auth-domain.firebaseapp.com",
+   databaseURL: "https://your-auth-domain.firebaseio.com",
+   projectId: "your-auth-domain",
+   storageBucket: "your-auth-domain.appspot.com",
+   messagingSenderId: "1234567890123"
+};
+firebase.initializeApp(config);
---
+++ public/main.js
@@ -0,0 +1,59 @@
+Vue.component('modal', {
+  template: '#modal-template'
+})
+
+var db = firebase.database();
+var myBooks = db.ref("/my/books/");
+var obj = {"books":[]};
+
+//初期表示&データ追加時
+myBooks.on("child_added", function(data) {
+  pushVal = data.toJSON();
+  pushVal.key = data.key;//key追加
+
+  obj.books.push(pushVal);
+  obj.books.sort();
+});
+//更新時
+myBooks.on("child_changed", function(data) {
+  for(i = 0;i < obj.books.length;i++){
+    if(obj.books[i]["ISBN13"] === data.val().ISBN13){
+      //console.log("一致しました。");
+      pushVal = data.toJSON();
+      pushVal.key = data.key;//key追加
+
+      obj.books[i] = pushVal
+      break;
+    }
+  }
+  obj.books.sort();//再描画のためにsortを呼び出す
+});
+
+//list出力
+var listVue = new Vue({
+  el: '#list',
+  data: {
+    books: obj.books,
+    showModal: false
+  },
+  methods:{
+    update: function(key,newTitle){
+      db.ref("/my/books/" + key)
+      .update({title: newTitle})
+      console.log("更新");
+    },
+
+    removeRecode: function(index,isbn13,key2){
+      if(window.confirm('「'+isbn13+'」のレコードを削除します。')){
+        db.ref("/my/books/" + key2)
+        .remove()
+        .then(function(index) {
+          obj.books.splice(index,1)
+          alert("削除しました");
+        }).catch(function(error) {
+          console.log("削除に失敗しました: " + error.message)
+        });
+      }
+    }
+  }
+});
